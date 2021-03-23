3435c3d37383df5c47e6585f361422c345087b3d
jietung <jietungchai@gmail.com>
2021-03-23 21:52:47 +0800

    Add triggers for part 4 tables

---
+++ proc.sql
@@ -0,0 +1,218 @@
+--Employee is either Manager, Administrator or Instructor
+
+--Adminstrator
+CREATE OR REPLACE FUNCTION admin_cant_be_mngr()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Managers
+    WHERE NEW.eid = Managers.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already a Manager';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER admin_cant_be_mngr
+BEFORE INSERT OR UPDATE 
+ON Administrators FOR EACH ROW
+EXECUTE FUNCTION admin_cant_be_mngr();
+
+CREATE OR REPLACE FUNCTION admin_cant_be_instr()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Instructors
+    WHERE NEW.eid = Instructors.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already an Instructor';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER admin_cant_be_instr
+BEFORE INSERT OR UPDATE 
+ON Administrators FOR EACH ROW
+EXECUTE FUNCTION admin_cant_be_instr();
+
+--Instructor
+CREATE OR REPLACE FUNCTION instr_cant_be_mngr()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Managers
+    WHERE NEW.eid = Managers.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already a Manager';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER instr_cant_be_mngr
+BEFORE INSERT OR UPDATE 
+ON Instructors FOR EACH ROW
+EXECUTE FUNCTION instr_cant_be_mngr();
+
+CREATE OR REPLACE FUNCTION instr_cant_be_admin()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Administrators
+    WHERE NEW.eid = Administrators.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already an Administrator';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER instr_cant_be_admin
+BEFORE INSERT OR UPDATE 
+ON Instructors FOR EACH ROW
+EXECUTE FUNCTION instr_cant_be_admin();
+
+--Manager
+CREATE OR REPLACE FUNCTION mngr_cant_be_admin()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Administrators
+    WHERE NEW.eid = Administrators.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already an Adminstrator';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER mngr_cant_be_admin
+BEFORE INSERT OR UPDATE 
+ON Managers FOR EACH ROW
+EXECUTE FUNCTION mngr_cant_be_admin();
+
+CREATE OR REPLACE FUNCTION mngr_cant_be_instr()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Instructors
+    WHERE NEW.eid = Instructors.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already an Instructor';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER mngr_cant_be_instr
+BEFORE INSERT OR UPDATE 
+ON Managers FOR EACH ROW
+EXECUTE FUNCTION mngr_cant_be_instr();
+
+--Employee is either Full-time Emp or Part-time Emp
+CREATE OR REPLACE FUNCTION PT_emp_cant_be_FT_emp()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Full_time_Emp
+    WHERE NEW.eid = Full_time_Emp.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already a Full-time Employer';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER PT_emp_cant_be_FT_emp
+BEFORE INSERT OR UPDATE 
+ON Part_time_Emp FOR EACH ROW
+EXECUTE FUNCTION PT_emp_cant_be_FT_emp();
+
+CREATE OR REPLACE FUNCTION FT_emp_cant_be_PT_emp()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Part_time_Emp
+    WHERE NEW.eid = Part_time_Emp.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already a Part-time Employer';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER FT_emp_cant_be_PT_emp
+BEFORE INSERT OR UPDATE 
+ON Full_time_Emp FOR EACH ROW
+EXECUTE FUNCTION FT_emp_cant_be_PT_emp();
+
+
+--Instructor is either Full-time instructor or Part-time instructor
+CREATE OR REPLACE FUNCTION PT_instr_cant_be_FT_instr()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Full_time_instructors
+    WHERE NEW.eid = Full_time_instructors.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already a Full-time Instructor';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER PT_instr_cant_be_FT_instr
+BEFORE INSERT OR UPDATE 
+ON Part_time_instructors FOR EACH ROW
+EXECUTE FUNCTION PT_instr_cant_be_FT_instr();
+
+CREATE OR REPLACE FUNCTION FT_instr_cant_be_PT_instr()
+RETURNS TRIGGER AS $$
+DECLARE count NUMERIC;
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Part_time_instructors
+    WHERE NEW.eid = Part_time_instructors.eid;
+    IF count > 0 THEN
+        RAISE NOTICE 'Employee is already a Part-time Instructor';
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER FT_instr_cant_be_PT_instr
+BEFORE INSERT OR UPDATE 
+ON Full_time_instructors FOR EACH ROW
+EXECUTE FUNCTION FT_instr_cant_be_PT_instr();
+
